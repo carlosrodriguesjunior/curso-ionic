c5bcb0341915a959d014d9f8acdc20653458551b
Carlos de Souza Rodrigues J'unior <carlosrodriguesjunior@macbook-air-2.fcamara.com.br>
2016-08-27 16:14:20 -0300

    final dia 1

--- www/modules/home/homeController.js
+++ www/modules/home/homeController.js
@@ -3,15 +3,34 @@ function HomeController( todoService, $scope) {
 this.taskList= [];
 var vm = this;
 
+this.removeTask =function(task){
 
-$scope.$on("$ionicView.beforeEnter", function(event, data){
+  todoService.removeTodo(task._id)
+  .then(function(result){
+    vm.loadTasks();
+  })
+  .catch(function(error){
+  console.log(error);
+});
+
+};
+
+this.loadTasks = function(){
+
   todoService.getTodo()
   .then(function(result){
    vm.taskList = result.data;
   })
   .catch(function(error){
   console.log(error);
-  });
+  })
+  .finally(function() {
+       $scope.$broadcast('scroll.refreshComplete');
+     });
+};
+
+$scope.$on("$ionicView.beforeEnter", function(event, data){
+  vm.loadTasks();
 });
 
 }
--- www/modules/home/todoService.js
+++ www/modules/home/todoService.js
@@ -8,6 +8,10 @@ this.getTodoById = function(id){
   return $http.get('http://fda8a136.ngrok.io/api/todo/'+id );
 }
 
+this.removeTodo = function(id){
+  return $http.delete('http://fda8a136.ngrok.io/api/todo/'+id );
+}
+
 }
 
 TodoService.$inject = ['$http'];
